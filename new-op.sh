724d07cd5bc2b1df1eac5e44fc36bfe3c90743e1
Ulisses Lima <ulisses@dvlcube.com>
2022-01-05 19:33:09 -0300

    improvements

--- new-op.sh
+++ new-op.sh
@@ -12,11 +12,16 @@ source $(real require.sh)
 
 query=$MYDIR/psql.sh
 
-kind="$1";      require --in 'BUY SELL' kind
+if [[ $# -lt 1 ]]; then
+  info "e.g.: $0 BUY 40 TICKER 677.85 USD '2020-12-02'"
+  exit 0
+fi
+
+kind="${1^^}";      require --in 'BUY SELL' kind
 amount="$2";    require -n amount
-ticker="$3";    require ticker
+ticker="${3^^}";    require ticker
 price="$4";     require -n price
-currency="$5";  require currency
+currency="${5^^}";  require currency
 created="$6";   [[ -z "$created" ]] && created=$(now.sh -d)
 inst="$7";      [[ -z "$inst" ]] && inst=undefined
 
@@ -31,9 +36,9 @@ if [[ -z "$asset_id" ]]; then
   asset_id=$($query "insert into assets (ticker_id, currency) values ($ticker_id, '$currency') returning id")
 fi
 
-asset_currency=$($query "select currency from assets where asset_id = $asset_id")
+asset_currency=$($query "select currency from assets where id = $asset_id")
 if [[ "$asset_currency" != "$currency" ]]; then
-  err "operation does not asset currency: $currency <> $asset_currency"
+  err "operation does not match asset currency: $currency <> $asset_currency"
   exit 4
 fi
 
@@ -43,33 +48,31 @@ id=$($query "insert into asset_ops (kind, asset_id, amount, price, currency, cre
 ")
 
 if [[ -n "$id" ]]; then
-  info success
+  info "success: $id"
   $query "select ticker.name, op.* from asset_ops op join assets a on a.id=op.asset_id join tickers ticker on ticker.id=a.ticker_id where op.id = $id" --full
 
   if [[ "$kind" == BUY ]]; then
-    $query "update assets set 
-      amount=amount+$amount, 
-      cost=cost+($price*$amount) 
+    $query "update assets set
+      amount=amount+$amount,
+      cost=cost+($price*$amount),
+      value=0
       where id = $asset_id
     "
   else
     # NOTE: there's no way to subtract the exact price, after a SELL operation
-    $query "update assets set 
-      amount=amount-$amount, 
-      cost=cost-($price*$amount) 
+    $query "update assets set
+      amount=amount-$amount,
+      cost=cost-($price*$amount),
+      value=0
       where id = $asset_id
     "
   fi
 
-  info "total:"
+  info "total cost:"
   inline-java.sh "println($price*$amount);"
+
+  # TODO get current value and calculate today's value
   
   info "current position:"
   $query "select * from assets where id = $asset_id" --full
 fi
-
-# TODO colocar os fundos nomad:
-# 345.13+207.22+125.5 = 677.85
-# criar uma tabela de portfolio, permitindo splittar por vários stocks, criar um script bulk, que já insere usando a estratégia de diversificação
-# nomad adventurer %: TLT 25.3 ISTB 17.1 XAR 9.8 FDN 9 IEMG 8.9 XSD 8.2 SPTL 6.6 RCD 6 IHY 5 RYT 1.6 XLI 1.3 RTH 1.2
-
