2b1fbee75aa303dea52b0b129c92418d82b3c0bf
Ulisses Lima <ulisses@dvlcube.com>
2022-01-08 19:08:50 -0300

    converters

---
+++ convert-brl-usd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# @installable
+# converts a BRL value to USD
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+source $MYDIR/env.sh
+[[ -f $LOCAL_ENV ]] && source $LOCAL_ENV
+source $MYDIR/log.sh
+source $(real require.sh)
+
+query=$MYDIR/psql.sh
+
+brl="$1"
+require brl "amount in BRL"
+
+exchange=$($MYDIR/scoop-rate.sh USD -x BRL | jq -r .response.rates.BRL)
+require exchange
+
+$query "select round(($brl/$exchange)::numeric, 2)"
---
+++ convert-usd-brl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# @installable
+# converts a BRL value to USD
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+source $MYDIR/env.sh
+[[ -f $LOCAL_ENV ]] && source $LOCAL_ENV
+source $MYDIR/log.sh
+source $(real require.sh)
+
+query=$MYDIR/psql.sh
+
+usd="$1"
+require usd "amount in USD"
+
+exchange=$($MYDIR/scoop-rate.sh USD -x BRL | jq -r .response.rates.BRL)
+require exchange
+
+$query "select round(($usd*$exchange)::numeric, 2)"
