09c07970f1217d8c701472402494bf5617493753
Ulisses Lima <ulisses@dvlcube.com>
2022-01-08 19:09:20 -0300

    new ticker

---
+++ new-ticker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+# @installable
+# adds a new ticker for snapshots
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+ME=$(basename $MYSELF)
+
+source $MYDIR/env.sh
+[[ -f $LOCAL_ENV ]] && source $LOCAL_ENV
+source $MYDIR/log.sh
+source $(real require.sh)
+
+query=$MYDIR/psql.sh
+
+if [[ $# -lt 1 ]]; then
+  info "e.g.: $0 IVVB11.SA"
+  exit 0
+fi
+
+ticker="${1^^}"
+require ticker
+
+public=${2:-true}
+
+ticker_id=$($query "select id from tickers where name iLIKE '${ticker}%' limit 1")
+if [[ -z "$ticker_id" ]]; then
+  ticker_id=$($query "insert into tickers (name) values ('$ticker') returning id")
+else
+  info "ticker already exists"
+fi
+
+info "ticker id: $ticker_id"
