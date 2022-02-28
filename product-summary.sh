e7cd6dd51a446e7b42435f6dc75bfa5557f2a91a
Ulisses Lima <ulisses@dvlcube.com>
2022-02-28 13:06:40 -0300

    variable period and interval

--- product-summary.sh
+++ product-summary.sh
@@ -18,6 +18,10 @@ shift
 
 brand="product.brand=product.brand"
 limit=5
+latest=$limit
+
+period="$(now.sh -y)-01-01"
+interval="('$period'::date+interval '1 year')"
 
 while test $# -gt 0
 do
@@ -29,6 +33,31 @@ do
     --limit|-l)
         shift
         limit="$1"
+    ;;
+    --latest)
+      shift
+      latest=$1
+    ;;
+    --period)
+      shift
+      period="$1"
+      type="$(count-char.sh "$period" '-')"
+      case "$type" in
+        0)
+          period="$period-$(now.sh -m)-01"
+          interval="('$period'::date+interval '1 year')"
+        ;;
+        1)
+          period="$period-01"
+          interval="('$period'::date+interval '1 month')"
+        ;;
+        2)
+          interval="('$period'::date+interval '1 month')"
+        ;;
+        *)
+          echo "bad period '$period'"
+        ;;
+      esac
     ;;
     -*)
         echo "bad option '$1'"
@@ -49,13 +78,14 @@ $query "select
   where (product.name = '${product_name}' or product.name iLIKE '%${product_name}%')
   and $brand
 order by op.created desc, op.id desc 
-limit $limit" --full
+limit $latest" --full
 
-info "average price:"
+info "average price considering period from '$period' $interval:"
 $query "select
     product.id,
     product.name, 
-    product.brand, 
+    product.brand,
+    round(avg(price), 2) as price,
     round((1 * sum(price) / sum(amount)), 2) as unit,
     product.extra->'carbs' \"carbs%\"
   from product_ops op
@@ -63,7 +93,8 @@ $query "select
   join stores store on store.id=op.store_id  
   where (product.name = '${product_name}' or product.name iLIKE '%${product_name}%')
   and $brand
-group by product.id 
+  and op.created between '$period' and $interval
+group by product.id, product.brand
 order by max(op.created) desc 
 " --full
 
