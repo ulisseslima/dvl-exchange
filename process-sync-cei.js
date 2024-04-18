ab04910f41f6496c94cf102860023308d702b92b
ulisses <ulisses@dvlcube.com>
2024-04-18 19:28:20 -0300

    terminal colors and process splits

--- process-sync-cei.js
+++ process-sync-cei.js
@@ -1,3 +1,31 @@
+const Reset = "\x1b[0m"
+const Bright = "\x1b[1m"
+const Dim = "\x1b[2m"
+const Underscore = "\x1b[4m"
+const Blink = "\x1b[5m"
+const Reverse = "\x1b[7m"
+const Hidden = "\x1b[8m"
+
+const FgBlack = "\x1b[30m"
+const FgRed = "\x1b[31m"
+const FgGreen = "\x1b[32m"
+const FgYellow = "\x1b[33m"
+const FgBlue = "\x1b[34m"
+const FgMagenta = "\x1b[35m"
+const FgCyan = "\x1b[36m"
+const FgWhite = "\x1b[37m"
+const FgGray = "\x1b[90m"
+
+const BgBlack = "\x1b[40m"
+const BgRed = "\x1b[41m"
+const BgGreen = "\x1b[42m"
+const BgYellow = "\x1b[43m"
+const BgBlue = "\x1b[44m"
+const BgMagenta = "\x1b[45m"
+const BgCyan = "\x1b[46m"
+const BgWhite = "\x1b[47m"
+const BgGray = "\x1b[100m"
+
 const fs = require('fs')
 const Pool = require('pg').Pool
 const dotenv = require('dotenv')
@@ -45,14 +73,15 @@ let itens = JSON.parse(json).itens
 			console.log(`	# ${movimentacao.instituicao}`)
 			console.log(`	${movimentacao.tipoMovimentacao} - ${tickerName}: ${movimentacao.quantidade} * ${movimentacao.precoUnitario} = ${movimentacao.valorOperacao}`)
 			if (!movimentacao.valorOperacao) {
-				console.log("	└ sync item ignored")
+				// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+				console.log(`${FgYellow}%s${Reset}`, `	└ sync item ignored. no value.`);
 				continue
 			}
 			
 			let results = await client.query(`select id from tickers where name ilike $1 limit 1`, [tickerName+"%"])
 			let ticker = results.rows[0]
 			if (!ticker || !ticker.id) {
-				console.log(`	└ !!! ticker not registered: ${tickerName}`)
+				console.log(`	└ ${BgMagenta}${FgWhite}!!! ticker not registered:${Reset} ${tickerName}`)
 			}
 
 			// TODO Grupamento. vem apenas o item 'quantidade'. ver se é a quantidade nova, ou o número da divisão
@@ -64,7 +93,7 @@ let itens = JSON.parse(json).itens
 
 			let tickerId = ticker.id
 			switch (tipoMovimentacao) {
-				case 'Empréstimo - Liquidação':
+				case 'Empréstimo - Liquidação': {
 					console.log(`	* processing loan (${tipoMovimentacao}) >`)
 					
 					// Às vezes não tem a quantidade de emprestados. checar na mesma movimentação se tem "quantidade" no 
@@ -97,9 +126,9 @@ let itens = JSON.parse(json).itens
 						'insert into loans (ticker_id, created, amount, total, currency) values ($1, $2, $3, $4, $5)', 
 						[tickerId, item.data, movimentacao.quantidade, movimentacao.valorOperacao, "BRL"]
 					)
-					console.log(`	└ loan saved`)
+					console.log(`	└ ${BgBlue}${FgWhite}loan saved${Reset}`)
 					break
-				case 'Rendimento':
+				} case 'Rendimento': {
 					console.log(`	* processing dividends (${tipoMovimentacao}) >`)
 
 					matches = await client.query(`
@@ -122,7 +151,7 @@ let itens = JSON.parse(json).itens
 					)
 					console.log(`	└ dividend saved`)
 					break
-				case 'Transferência - Liquidação':
+				} case 'Transferência - Liquidação': {
 					console.log(`	* processing ops >`)
 
 					let assets = await client.query(`select id from assets where ticker_id = $1`, [ticker.id])
@@ -155,9 +184,42 @@ let itens = JSON.parse(json).itens
 					console.log(`	└- asset updated`)
 
 					break
-				default:
-					console.log(`	└ type ignored: ${tipoMovimentacao}`)
+				} case 'Grupamento': case 'Desdobro': {
+					console.log(`	* processing split >`)
+
+					let assets = await client.query(`select id from assets where ticker_id = $1`, [ticker.id])
+					let asset = assets.rows[0]
+
+					amount = await client.query(`
+						select amount from assets
+						where id = $1
+					`, [asset.id])
+
+					if (amount == (amount+(movimentacao.quantidade))) {
+						console.log("	└ split already saved")
+						continue
+					}
+
+					await client.query(
+						`update assets set amount=$1 where id=$2`,
+						[(amount+(movimentacao.quantidade)), asset.id]
+					)
+					console.log(`	└- split updated`)
+
+					await client.query(`
+						insert into splits 
+						(asset_id, ticker_id, old_amount, new_amount, reverse) 
+						values ($1, $2, $3, $4, $5)
+					`, [asset.id, ticker.id, amount, (amount+(movimentacao.quantidade)), ((amount+(movimentacao.quantidade))<amount)]
+					)
+					console.log(`	└- ${BgCyan}${FgBlue}split registered from ${amount} to ${movimentacao.quantidade}${Reset}`)
+			
 					break
+				} default: {
+					// console.log(`	└ type ignored: ${tipoMovimentacao}`)
+					console.log(`${BgYellow}${FgBlack}%s${Reset}`, `	└ type ignored: ${tipoMovimentacao}`);
+					break
+				}
 			}
 		}
 
