099acf35305ff68efc8bdd596ea009d1253c7f19
ulisses <ulisses@dvlcube.com>
2024-06-28 01:03:38 -0300

    get curr ticker price correctly

--- price.sh
+++ price.sh
@@ -17,11 +17,17 @@ ticker="${1^^}"
 require ticker
 shift
 
+date=$(now.sh -dt)
+
 while test $# -gt 0
 do
   case "$1" in
     --full)
       mode='--full'
+    ;;
+    --date|-d)
+      shift
+      date="$1"
     ;;
     -*)
       echo "$(sh_name $ME) - bad option '$1'"
@@ -30,11 +36,15 @@ do
   shift
 done
 
+info "date: $date"
 $query "select
-  price(ticker.id),
-  max(snap.currency) currency
+  snap.price,
+  snap.currency
 from snapshots snap
 join tickers ticker on ticker.id=snap.ticker_id
 where ticker.name like '${ticker}%'
-group by ticker.id
+and snap.created::date <= '$date'::date
+group by ticker.id, snap.id
+order by snap.created desc
+limit 1
 " $mode
